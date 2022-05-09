67ff129dc094551202322c375cdc7c2ee10dfd80
Александр Немеров <nemerov_a@mail.ru>
2022-05-10 01:17:02 +0800

    ор

--- LabW_1_2_8_Progs/SecondGeneralTask.cpp
+++ LabW_1_2_8_Progs/SecondGeneralTask.cpp
@@ -3,177 +3,68 @@
 
 #include "Libraries.h"
 
-
-void inputGeneral(string &name, string &companyName, int &price, int &weight) {
-	cout << "\n\n\tНазвание: ";
-	cin >> name;
-	cout << "\n\tНазвание компании: ";
-	cin >> companyName;
-	cout << "\n\tЦена: ";
-	cin >> price;
-	cout << "\n\tВес: ";
-	cin >> weight;
-}
-
-void newProduct(Product*& Current, Product*& Head, string name, string companyName, 
-	int price, int weight, bool open, int expirationDateInCloseState,
-	int expirationDateInOpenState, int expirationDate, string typeOfComponent, string productType) {
-	if (Current == NULL) {
-		Current = new Product(name, companyName, price, weight, open, expirationDateInCloseState,
-			expirationDateInOpenState, expirationDate, typeOfComponent, productType);
-		Head = Current;
+//При создании ключа, не нумеруются строки
+class KeyGen {
+public:
+	virtual string get() = 0;
+	static KeyGen* random(int n);
+	static KeyGen* order(string str);
+};
+class Rand : public KeyGen{
+public:
+	Rand() {}
+	Rand(int n) {
+		this->n = n;
+		size += 1;
 	}
-	else if (Current->next == NULL) {
-		Current->next = new Product(name, companyName, price, weight, open, expirationDateInCloseState,
-			expirationDateInOpenState, expirationDate, typeOfComponent, productType);
-		Current = Current->next;
-	}
-}
-void secondGeneralTask()
-{
-	int counter = 0;
-	const int size = 10;
-	Product* products = NULL;
-	Product* head = NULL;
-	/*Product* products = static_cast<Product*>(operator new[](size * sizeof(Product)));
-	//Product** products = new Product*[size];
-	for (int i = 0; i < size; i++)
-		new (products + i) Product();
-		//products[i] = new Product();*/
-	while (true) {
-		system("cls");
-		string	productType					= { "None" };
-		string	typeOfComponent				= { "None" };
-		string	companyName					= { "None" };
-		string	name						= { "None" };
-		int		expirationDateInCloseState	= 0;
-		int		expirationDateInOpenState	= 0;
-		int		expirationDate				= 0;
-		int		price						= 0;
-		int		weight						= 0;
-		bool	open						= false;
-		cout << "\n\t1 - Добавить новый продукт"
-			<< "\n\t2 - Вывести все продукты"
-			<< "\n\t0 - Выход\n\t";
-		int inputNum;
-		cin >> inputNum;
-		system("cls");
-		switch (inputNum) {
-			case (0): return;
-			case (1): {
-				cout << "\n\tДобавить Товар"
-					<< "\n\t1 - Продукт питания"
-					<< "\n\t2 - Электронный товар"
-					<< "\n\t0 - Выход\n\t";
-				cin >> inputNum;
-				system("cls");
-				switch (inputNum) {
-					case (0): return;
-					case (1): {
-						cout << "\n\tДобавить продукт питания"
-							<< "\n\t1 - Консервированный продукт"
-							<< "\n\t2 - Свежий продукт"
-							<< "\n\t0 - Выход\n\t";
-						cin >> inputNum;
-						system("cls");
-						switch (inputNum) {
-							case (0): return;
-							case (1): {
-								cout << "\n\tДобавить консервированный продукт";
-								inputGeneral(name, companyName, price, weight);
-								cout << "\n\tОткрыта ли консервы: \n\t1 - Открыта\n\t2 - Закрыта\n\t0 - Выход\n\t";
-								cin >> inputNum;
-								switch (inputNum) {
-									case(0): return;
-									case(1): {
-										open = true;
-										cout << "\n\tСрок годности(открытой консервы): ";
-										cin >> expirationDateInOpenState;
-										break;
-									}
-									case(2): {
-										open = false;
-										cout << "\n\tСрок годности(закрытой консервы): ";
-										cin >> expirationDateInCloseState;
-										break;
-									}
-									default: {
-										cout << "\n\tОшибка ввода \n\n\t";
-										system("pause");
-										continue;
-									}
-								}
-								newProduct(products, head, name, companyName, price, weight,
-									open, expirationDateInCloseState, expirationDateInOpenState,
-									expirationDate, typeOfComponent, productType);
-								counter++;
-								break;
-							}
-							case (2): {
-								cout << "\n\tДобавить свежий продукт";
-								inputGeneral(name, companyName, price, weight);
-								cout << "\n\tСрок годности: ";
-								cin >> expirationDate;
-								newProduct(products, head, name, companyName, price, weight,
-									open, expirationDateInCloseState,expirationDateInOpenState, 
-									expirationDate, typeOfComponent, productType);
-								counter++;
-								break;
-							}
-							default: {
-								cout << "\n\tОшибка ввода \n\n\t";
-								system("pause");
-								continue;
-							}
-						}
-						break;
-					}
-					case (2): {
-						cout << "\n\tДобавить электронный продукт";
-						inputGeneral(name, companyName, price, weight);
-						cout << "\n\tТип продукта: ";
-						cin >> productType;
-						cout << "\n\tТип комплетующего: ";
-						cin >> typeOfComponent;
-						newProduct(products, head, name, companyName, price, weight,
-							open, expirationDateInCloseState, expirationDateInOpenState,
-							expirationDate, typeOfComponent, productType);
-						counter++;
-						break;
-					}
-					default: {
-						cout << "\n\tОшибка ввода \n\n\t";
-						system("pause");
-						continue;
-					}
-				}
-				break; 
-			}
-			case (2): {
-				products = head;
-				if (head == NULL) {
-					cout << "\n\tEmpty\n\t";
-					system("pause");
-					break;
-				}
-				int days = 0;
-				while (true) {
-					string temp = *products;
-					cout << "\n\t" << temp;
-					days += products->getExpirationDate();
-					if (products->next == NULL)break;
-					products = products->next;
-				}
-				cout << "\n\tОбщая сумма сроков годности: " << days << "\n\n\t";
-				system("pause");
-				continue;
-			}
-			default: {
-				cout << "\n\tОшибка ввода \n\n\t";
-				system("pause");
-				continue;
-			}
+	string get() override {
+		string str;
+		srand(time(NULL));
+		for (int i = 0; i < n; i++){
+			str += to_string(0 + rand() % 9);
 		}
+		return str;
 	}
+	friend KeyGen* KeyGen::random(int n);
+	int GetN() {
+		return n;
+	}
+protected:
+	int n, size;
+};
+class Order : KeyGen, Rand {
+public:
+	Order() {}
+	Order(string str) {
+		this->str = str;
+	}
+	string get() override {
+		str += to_string(size);
+		return str;	
+	}
+	friend KeyGen* KeyGen::order(string str);
+private:
+	string str;
+};
+KeyGen* KeyGen::random(int n){
+	return static_cast<KeyGen*>(new Rand(n));
+}
+KeyGen* KeyGen::order(string str) {
+	return static_cast<KeyGen*>(new Order(str));
+}
+
+void secondGeneralTask() {
+	KeyGen* a;
+	a = KeyGen::order("str");
+	cout << a->get() << " : ";
+	a = KeyGen::random(4);
+	cout << a->get() << endl;
+	cout << "*******************************" << endl;
+	a = KeyGen::order("Key");
+	cout << a->get() << " : ";
+	a = KeyGen::random(5);
+	cout << a->get() << endl;
+	system("pause");
+	system("cls");
 	return;
 }
